ea7580db25fad6b00cb99b892363923ac5bbaac5
menkaix <mendrika.irina@gmail.com>
2025-10-18 15:13:26 +0300

    changed port

--- src/main/java/com/menkaix/smartlog/SmartlogApplication.java
+++ src/main/java/com/menkaix/smartlog/SmartlogApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "com.menkaix.smartlog")
+@SpringBootApplication(scanBasePackages = {
+		"com.menkaix.smartlog.controller",
+		"com.menkaix.smartlog.service"
+})
 @EnableJpaRepositories("com.menkaix.smartlog.repository")
 @EntityScan("com.menkaix.smartlog.model")
 public class SmartlogApplication {
